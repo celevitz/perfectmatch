ce87fa3ef8ad7dcf544d7463c087d9efde1fe255
Carly <celevitz@gmail.com>
2024-06-09 12:07:22 -0700

    Comment the code

--- dev/BringInData.R
+++ dev/BringInData.R
@@ -7,26 +7,27 @@ library(tidyverse)
 
 directory <- "/Users/carlylevitz/Documents/Data/"
 
-contestants <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
-                                   ,sheet=1))
-matches <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
-                                   ,sheet=2))
-boardroom <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
-                                   ,sheet=3))
-boardroomoptions <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
-                                   ,sheet=4))
-games <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
-                                   ,sheet=5))
-voting <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
-                                   ,sheet=6))
+# Bring in the data
+  contestants <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
+                                     ,sheet=1))
+  matches <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
+                                     ,sheet=2))
+  boardroom <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
+                                     ,sheet=3))
+  boardroomoptions <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx"
+                                                ,sep=""),sheet=4))
+  games <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
+                                     ,sheet=5))
+  voting <- as_tibble(read.xlsx(paste(directory,"PerfectMatch.xlsx",sep="")
+                                     ,sheet=6))
 
-
-save(contestants, file = "data/contestants.rda")
-save(matches, file = "data/matches.rda")
-save(boardroom, file = "data/boardroom.rda")
-save(boardroomoptions, file = "data/boardroomoptions.rda")
-save(games, file = "data/games.rda")
-save(voting, file = "data/voting.rda")
+# Save the data files
+  save(contestants, file = "data/contestants.rda")
+  save(matches, file = "data/matches.rda")
+  save(boardroom, file = "data/boardroom.rda")
+  save(boardroomoptions, file = "data/boardroomoptions.rda")
+  save(games, file = "data/games.rda")
+  save(voting, file = "data/voting.rda")
 
 
 
